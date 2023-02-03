0537451ae53dd25507b4980ac7cbd12d97245d41
Danny <dosullivan557@gmail.com>
2023-02-03 22:15:44 +0000

    use validated hook with regeex

---
+++ src/useValidatedForm.js
@@ -0,0 +1,13 @@
+const useValidatedForm = (regex) => {
+  const [value, setValue] = useState("");
+  const [isValid, setIsValid] = useState(false);
+
+  useEffect(() => {
+    const res = regex.test(value);
+    setIsValid(res);
+  }, [value, regex]);
+
+  return [value, setValue, isValid];
+};
+
+export default useValidatedForm;
